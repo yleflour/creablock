60ed688bd2eb70770fc0e743b44e4d4f9e624330
yleflour <yleflour@gmail.com>
2013-05-07 17:40:27 +0200

    Possibility to add/remove block (within limit)

--- js/game/components.js
+++ js/game/components.js
@@ -4,55 +4,99 @@ define(['crafty'], function(Crafty) {
 		Game = {
 			width: 600,
 			height: 400,
-			nbBlocks: 10,
+            offset:{
+                x: 0,
+                y: 0
+            },
+			nbBlocks: 12,
 			blockWidth: 32,
 			blockHeight: 32
 		};
         
         State = {
+            nbBlocks: 0,
+           nbInstanciated: 0,
             width: 0,
             height: 0,
             content: []
         };
         
+        Puzzle = undefined;
+        
+        //Canvas click detection
+        Crafty.c('GhostBlock', {
+            
+            init: function(){
+                this.requires('2D, Canvas, Mouse, Color, Grid')
+                    .attr({
+                        w: Game.blockWidth,
+                        h: Game.blockHeight
+                    })
+                    .bind('MouseOver', this.show)
+                    .bind('MouseOut', this.hide)
+                    .bind('Click', this.onClick)
+                    .hide();
+            },
+            
+            show: function(event){
+                this.color('#314122');
+                return this;
+            },
+                    
+            hide: function(event){
+                this.color('black');
+                return this;
+            },
+            
+            onClick: function(event){
+                if(event.mouseButton === Crafty.mouseButtons.LEFT){
+                    Puzzle.newBlock(this.at());
+                }
+            }
+        });
+        
 		//Main logic
 		Crafty.c('Puzzle', {
-
+            
+            _ghosts : [],
+            
 			init: function(){
-				this.requires('2D, Canvas');
-
-				//State Initiation
+                //Creation
+				this.requires('2D, Canvas, Dropping');
+                Puzzle = this;
+                
+                //Add background
+                Crafty.e('Background');
+                
+				//State Initialization
 				for(var i = 0; i < Game.nbBlocks; i++){
 					State.content[i] = [];
 					for(var j = 0; j < Game.nbBlocks; j++)
 						State.content[i][j] = undefined;
 				}
 				
-				//Blocks creation
-				for(var i = 0; i < Game.nbBlocks; i++)
-					this.newBlock({x: i, y: 0}, i);
-
-				//Center stage
-				this.refresh();
-                console.log("done");
+                //First Blocks
+                for(var i = 0; i < 2; i++)
+                    this.newBlock({x: i, y: 0});
 			},
-			
-			newBlock: function(pos, id){
-                var block = Crafty.e('Block').block(id);
-				State.content[pos.x][pos.y] = block;
+            
+			newBlock: function(pos){
+                State.nbBlocks ++;
+                State.nbInstanciated ++;
+                
+                var block = Crafty.e('Block, DraggableBlock')
+                    .block(State.nbInstanciated)
+                    .disableBlockDrag();
 				this.attach(block);
-				if(pos.x >= State.width)
-					State.width ++;
-				if(pos.y >= State.height)
-					State.height ++;
-				block.at(pos);
-
+				block.place(pos);
+                
 				return this;
 			},
 
 			refresh: function(){
 				this.centerGrid();
 				this.checkDraggables();
+                this.refreshGhosts();
 			},
 
 			centerGrid: function(){
@@ -61,15 +105,35 @@ define(['crafty'], function(Crafty) {
 					y: (Game.height - Game.blockHeight * State.height) / 2
 				});
 			},
-
+            
+            refreshGhosts: function(){
+                this.removeGhosts();
+                
+                //New ghosts
+                this._ghosts = [];
+                
+                if(State.nbInstanciated < Game.nbBlocks)
+                    for(var x = -1; x <= State.width; x++)
+                        for(var y = -1; y <= State.height; y++)
+                            if(this.isDroppable({x:x, y:y}))
+                                this._ghosts.push(Crafty.e('GhostBlock').at({x:x, y:y}));
+                            
+            },
+            
+            removeGhosts: function(){
+                //Delete ghosts
+                for(var i in this._ghosts)
+                    this._ghosts[i].destroy();
+            },
+            
 			save: function(){
 				for(var y = 0; y < Game.nbBlocks; y++){
 					var line = "";
 					for(var x = 0; x < Game.nbBlocks; x++){
 						if(State.content[x][y] === undefined)
-							line += "0";
+							line += "0 ";
 						else
-							line += "1";
+							line += "1 ";
 					}
 					console.log(line);
 				}
@@ -104,26 +168,31 @@ define(['crafty'], function(Crafty) {
 									}
 								}
 					};
-
-					if(count._id === 0)
+                    
+					if(block._id === 0)
 						recursiveCheck(self._children[1]);
 					else
 						recursiveCheck(self._children[0]);
 
 					//Output
-					if(count === Game.nbBlocks)
+					if(count === self._children.length)
 						return true;
 					else
 						return false;
 				};
 
-				//Testing purposes
-				for(var i = 0; i < Game.nbBlocks; i++){
-					if(isDraggable(this._children[i]))
-						this._children[i].requires('DraggableBlock');
-                    else
-						this._children[i].removeComponent('DraggableBlock');
-                }
+				//Enabling Disabling
+                if(this._children.length <=2)
+                    for(var i in this._children)
+                        this._children[i].enableBlockDrag();
+                        
+                else
+                    for(var i in this._children){
+                        if(isDraggable(this._children[i]))
+                            this._children[i].enableBlockDrag();
+                        else
+                            this._children[i].disableBlockDrag();
+                    }
                         
 			},
 
@@ -252,6 +321,7 @@ define(['crafty'], function(Crafty) {
 					break;
 				}
 			}
+            
 		});
 
 		//Blocks Logic
@@ -264,118 +334,166 @@ define(['crafty'], function(Crafty) {
 			},
 
 			init: function(){
-				this.requires("Canvas, 2D, Color, Mouse")
-				.attr({w: Game.blockWidth, h: Game.blockHeight})
-				.color('rgb(255,255,255)')
-				.bind('Click', this.onClick);
-			},
-
-			at: function(pos){
-				if (pos === undefined){
-					return {
-						x: Math.floor((this.x - this._parent.x + Game.blockHeight / 2) / Game.blockWidth),
-						y: Math.floor((this.y - this._parent.y + Game.blockHeight / 2) / Game.blockHeight)
-					};
-				}
-				else {
-					this.attr({
-						x: (this._parent.x + pos.x * Game.blockWidth),
-						y: (this._parent.y + pos.y * Game.blockHeight)
-					});
-
-					return this;
-				}
-			},
-
-			onClick: function(event){
-				var pos = this.at();
+				this.requires("Canvas, 2D, Color, Grid")
+				.attr({w: Game.blockWidth, h: Game.blockHeight});
 			}
+            
 		});
 
 		Crafty.c('DraggableBlock', {
 			_initialPos : {},
+            _draggableBlock : false,
 
 			init: function(){
-				this.requires("Draggable")
-				.bind('StartDrag', this.dragStart)
-				.bind('Dragging', this.drag)
-				.bind('StopDrag', this.dragEnd);
-			},
+                this
+                .requires('Draggable, Dropping, Mouse')
+                .bind('MouseOver', this.onMouseOver)
+                .bind('MouseOut', this.onMouseOut)
+                .bind('MouseUp', this.onClick)
+                .disableDrag()
+                .color('rgb(255,255,255)');
+            },
+            
+            enableBlockDrag: function(){
+                if(!this._draggableBlock){
+                    this.enableDrag();
+                    this.bind('StartDrag', this.onDragStart);
+                    this.bind('Dragging', this.onDrag);
+                    this.bind('StopDrag', this.onDragEnd);
 
-			dragStart: function(event){
+                    this._draggableBlock = true;
+                }
+                return this;
+            },
+            
+            disableBlockDrag: function(){
+                if(this._draggableBlock){
+                    this.unbind('StartDrag', this.onDragStart);
+                    this.unbind('Dragging', this.onDrag);
+                    this.unbind('StopDrag', this.onDragEnd);
+                    this.disableDrag();
+                
+                    this._draggableBlock = false;
+                }
+                return this;
+            },
+            
+			onDragStart: function(event){
+                Puzzle.removeGhosts();
                 var pos = this._initialPos = this.at();
-                console.log(this._initialPos);
 				State.content[pos.x][pos.y] = undefined;
 				if(pos.x === 0){
-					if(this._parent.shiftRemove("left"))
+					if(Puzzle.shiftRemove("left"))
                         this._initialPos.x--;
                 }
 				if(pos.y === 0){
-					if(this._parent.shiftRemove("up"))
+					if(Puzzle.shiftRemove("up"))
                         this._initialPos.y--;
                 }
-				this._parent.shiftRemove();
+				Puzzle.shiftRemove();
 			},
 
-			drag: function(event){
+			onDrag: function(event){
                 //console.log(this.at());
 				if(this.isDroppable()){
 					this.at(this.at());
 				}
 			},
 
-			dragEnd: function(event){
+			onDragEnd: function(event){
 				var pos = {};
 				
 				//If droppable
 				if(this.isDroppable()){
-                    //console.log("droppable");
 					pos = this.at();
                 }
 				//If not, replace
 				else{
-                    //console.log("Not droppable");
-                    console.log(this._initialPos);
                     pos = this._initialPos;
                 }
                 
-				//Shifts//
+                this.place(pos);
+				
+			},
+            
+            onClick: function(event){
+                if(event.mouseButton === Crafty.mouseButtons.RIGHT && this._draggableBlock){
+                    var pos = this.at();
+                    
+                    //Remove from grid
+                    State.content[pos.x][pos.y] = undefined;
+                    State.nbInstanciated --;
+                    
+                    //Remove empty spaces
+                    if(pos.x === 0){
+                        if(Puzzle.shiftRemove("left"))
+                            this._initialPos.x--;
+                    }
+                    if(pos.y === 0){
+                        if(Puzzle.shiftRemove("up"))
+                            this._initialPos.y--;
+                    }
+                    Puzzle.shiftRemove();
+                    
+                    //Destroys
+                    Puzzle.refresh();
+                    this.destroy();
+                }
+			},
+            
+            place: function(pos){
+                //Shifts//
 				//If to the left edge
 				if(pos.x === -1){
-                    this._parent.shiftAdd("left");
+                    Puzzle.shiftAdd("left");
                     pos.x = 0;
 				}
 
 				//If to the top edge
 				if(pos.y === -1){
-                    this._parent.shiftAdd("up");
+                    Puzzle.shiftAdd("up");
                     pos.y = 0;
 				}
 
 				//If to the top edge
 				if(pos.x === State.width){
-                    this._parent.shiftAdd("right");
+                    Puzzle.shiftAdd("right");
 				}
 
 				//If to the top edge
 				if(pos.y === State.height){
-                    this._parent.shiftAdd("down");
+                    Puzzle.shiftAdd("down");
 				}
 
-				//Add element
+				//Move element
 				this.at(pos);
 				State.content[pos.x][pos.y] = this;
+                
+				Puzzle.refresh();
+            },
+            
+            onMouseOver: function(event){
+                if(this._draggableBlock) 
+                    this.color('rgb(110,110,110)');
+                else
+                    this.color('rgb(210,210,210)');
+            },
+                    
+            onMouseOut: function(event) {
+                this.color('rgb(255,255,255)');
+            }
 
-				this._parent.checkDraggables();
-			},
-
-			isDroppable: function(){
-				var pos = this.at();
+		});
+        
+        //Reusable methods
+        Crafty.c('Dropping', {
+            isDroppable: function(pos){
+				if(pos === undefined)
+                    pos = this.at();
 
 				//Space not already taken
 				if(Crafty.math.withinRange(pos.x, 0, Game.nbBlocks - 1) && Crafty.math.withinRange(pos.y, 0, Game.nbBlocks - 1))
 					if(State.content[pos.x][pos.y] !== undefined){
-                        //console.log("Taken");
 						return false;
                     }
 
@@ -386,16 +504,30 @@ define(['crafty'], function(Crafty) {
 							if( Crafty.math.withinRange(pos.x + x, 0, Game.nbBlocks - 1) 
 						   		&& Crafty.math.withinRange(pos.y + y, 0, Game.nbBlocks - 1)
 								&& State.content[pos.x + x][pos.y + y] !== undefined){
-                                    //console.log("Found neighbour");
 									return true;
                                 }
-                //console.log("Nada");
+                                
 				return false;
-			},
-
-			place: function(){
-				
 			}
-		});
+        });
+        
+        Crafty.c('Grid', {
+            at: function(pos){
+				if (pos === undefined){
+					return {
+						x: Math.floor((this.x - Puzzle.x + Game.blockHeight / 2) / Game.blockWidth),
+						y: Math.floor((this.y - Puzzle.y + Game.blockHeight / 2) / Game.blockHeight)
+					};
+				}
+				else {
+					this.attr({
+						x: (Puzzle.x + pos.x * Game.blockWidth),
+						y: (Puzzle.y + pos.y * Game.blockHeight)
+					});
+
+					return this;
+				}
+			}
+        });
 	};
 });
--- js/game/game.js
+++ js/game/game.js
@@ -4,8 +4,11 @@ define(['crafty', 'game/components', 'jquery'], function(Crafty, Components, $) 
 	game.start = function(){
 		Components();
 		var cra = Crafty.init(600, 400);
+        /*Crafty.modules({ 'crafty-debug-bar': 'release' }, function () {
+            Crafty.debugBar.show();
+        });*/
 		Crafty.background('rgb(0,0,0)');
-		var puzzle = Crafty.e("Puzzle")
+		var puzzle = Crafty.e("Puzzle");
 	};
 
 	return game;
